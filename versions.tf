58e937f40dec3aaf3bb832d6a77549b037b28af0
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-10-18 21:17:25 +0200

    Remove providers

--- versions.tf
+++ versions.tf
@@ -4,16 +4,6 @@ terraform {
       source  = "philips-software/hsdp"
       version = ">= 0.23.2"
     }
-    cloudfoundry = {
-      source = "cloudfoundry-community/cloudfoundry"
-    }
-    twilio = {
-      source  = "RJPearson94/twilio"
-      version = ">= 0.16.0"
-    }
-    htpasswd = {
-      source = "loafoe/htpasswd"
-    }
   }
 }
 
