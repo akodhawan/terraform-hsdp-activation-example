42fdb713393298e76a75724fe95867873c0fe6ee
Akhil <akhil.dhawan@philips.com>
2021-11-08 15:54:09 +0100

    Executed based on logins rather tahn state - updated main tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 resource "hsdp_iam_activation_email" "active" {
-  for_each = toset(flatten(concat([],data.terraform_remote_state.local.outputs.unverified_user_ids.*.ids)))
-
+  for_each = toset(flatten(data.hsdp_iam_users.unverified.*.logins)) //toset(data.terraform_remote_state.local.outputs.unverified_user_ids)
   user_id = each.key
-
   resend_every = 1
 }
