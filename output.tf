f4c4d082c267c15f485cc99388af6bab7721ef4f
Akhil <akhil.dhawan@philips.com>
2021-11-08 15:54:26 +0100

    Added outputs

--- output.tf
+++ output.tf
@@ -6,3 +6,10 @@ output "last_activations_emails" {
   value = hsdp_iam_activation_email.active
 }
 
+output "unverified_users" {
+  value = data.hsdp_iam_users.unverified
+}
+
+output "unverified_users_ids" {
+  value = data.terraform_remote_state.local.outputs.unverified_user_ids
+}
